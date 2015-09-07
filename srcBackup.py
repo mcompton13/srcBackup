7325283b7d30ab81b700c197fb7de39b172a6cb4
Matthew Compton <comptonma@gmail.com>
2015-09-06 21:40:17 -0700

    More robust syncing of files, creates archives of unpushed changes.

--- srcBackup.py
+++ srcBackup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import argparse
-from os import path
+from datetime import datetime
+from dateutil.parser import parse
+from os import makedirs, path
 import subprocess
 import sys
 from tempfile import NamedTemporaryFile
@@ -9,6 +11,14 @@ from tempfile import NamedTemporaryFile
 
 INCLUDE_FILE_FORMAT = '+ {}\n'
 INCLUDE_DIR_FORMAT = '+ {}/\n'
+EXCLUDE_ALL = '- *\n'
+
+BUNDLE_FILENAME_FORMAT = 'bundle_{}_{}.hg'
+
+HG_OUTGOING_TEMPLATE_FORMAT = '{}{{node|short}}\\n{}{{date|rfc3339date}}'
+
+HG_OUTGOING_NODE_MARKER = ':NODE:'
+HG_OUTGOING_DATE_MARKER = ':DATE:'
 
 
 is_verbose = False
@@ -28,47 +38,158 @@ def error(message):
     sys.stderr.write(message + '\n')
 
 
+def hg_list_uncommitted_files(repo_dir_path, include_removed=False, include_deleted=False):
+
+    args = [ "hg",
+        "status",
+        "--repository",
+        repo_dir_path,
+        "--no-status",
+        "--added",
+        "--modified",
+        "--unknown",
+    ]
+
+    if is_verbose:
+        args += ["--verbose"]
+    else:
+        args += ["--quiet"]
+
+    if include_removed:
+        args += ["--removed"]
+
+    if include_deleted:
+        args += ["--deleted"]
+
+    debug("Calling hg: " + str(args))
+
+    result = subprocess.check_output(args).strip().split('\n')
+
+    debug(result)
+
+    return result
+
+
+def to_utc_datetime(local_datetime):
+    timezoneless_datetime = datetime(
+        local_datetime.year, local_datetime.month, local_datetime.day, local_datetime.hour,
+        local_datetime.minute, local_datetime.second, local_datetime.microsecond, tzinfo=None)
+
+    utc_datetime = timezoneless_datetime
+
+    if local_datetime.utcoffset() != None:
+        utc_datetime = timezoneless_datetime - local_datetime.utcoffset()
+
+    return utc_datetime
+
+
+def hg_newest_unpushed_commit(repo_dir_path):
+
+    args = [ "hg",
+        "outgoing",
+        "--repository",
+        repo_dir_path,
+        "--newest-first",
+        "--limit", "1",
+        "--template",
+        str.format(HG_OUTGOING_TEMPLATE_FORMAT, HG_OUTGOING_NODE_MARKER, HG_OUTGOING_DATE_MARKER),
+    ]
+
+    if is_verbose:
+        args += ["--verbose"]
+    else:
+        args += ["--quiet"]
+
+    debug("Calling hg: " + str(args))
+
+    output_lines = subprocess.check_output(args).strip().split('\n')
+
+    rev_hash = None
+    rev_utc_datetime = None
+
+    for line in output_lines:
+        if line.startswith(HG_OUTGOING_NODE_MARKER):
+            rev_hash = line[len(HG_OUTGOING_NODE_MARKER):]
+
+        if line.startswith(HG_OUTGOING_DATE_MARKER):
+            date_str = line[len(HG_OUTGOING_DATE_MARKER):]
+
+            rev_utc_datetime = to_utc_datetime(parse(date_str))
+
+
+    debug(str.format('Most recent unpushed commit: rev: {}\tdate: {}', rev_hash, rev_utc_datetime))
+
+    return rev_hash, rev_utc_datetime
+
+
+def hg_bundle_unpushed_commits(repo_dir_path, destination_bundle_filename):
+
+    destination_dir_path = path.dirname(destination_bundle_filename)
+    if not path.exists(destination_dir_path):
+        makedirs(destination_dir_path, 0777)
+
+    args = [ "hg",
+        "bundle",
+        "--repository",
+        repo_dir_path,
+    ]
+
+    if is_verbose:
+        args += ["--verbose"]
+    else:
+        args += ["--quiet"]
+
+    args += [destination_bundle_filename]
+
+    debug("Calling hg: " + str(args))
+
+    result = subprocess.check_output(args).strip().split('\n')
+
+
+
+def make_hg_bundle_filename(commit_rev_hash, commit_datetime_utc):
+    result = str.format(BUNDLE_FILENAME_FORMAT, commit_datetime_utc.isoformat(), commit_rev_hash)
+
+    debug('Bundle filename: ' + result)
+
+    return result
+
+
+
 def rsync(source_dir_path, source_files, destination_dir_path):
 
     with NamedTemporaryFile() as tmpfile:
 
         source_dirs = []
         for source_file in source_files:
-            #print str.format(INCLUDE_DIR_FORMAT, path.dirname(source_file))
-            #print str.format(INCLUDE_FILE_FORMAT, source_file)
-
             tmpfile.write(str.format(INCLUDE_DIR_FORMAT, path.dirname(source_file)))
             tmpfile.write(str.format(INCLUDE_FILE_FORMAT, source_file))
 
 
+        # Add line to exclude all files that were NOT included above
+        tmpfile.write(EXCLUDE_ALL)
+
+        tmpfile.flush()
+
         args = ["rsync",
             "--archive",
+            "--verbose",
+           # "--dry-run",
             "--delete",
             "--delete-excluded",
-           # "--include-from",
-           # tmpfile.name,
-            "--include",
-            str.format("/{}/", path.dirname(source_files[0])),
-            "--include",
-            str.format("{}", source_files[0]),
-            "--exclude",
-            "*",
-            "--verbose",
-            "--dry-run",
+            "--include-from",
+            tmpfile.name,
             source_dir_path,
             destination_dir_path
             ]
 
         debug("Calling rsync: " + str(args))
 
-        print
-        print
-
         result = subprocess.call(args)
 
 
 def main(argv):
-    print 'argv=%s' % argv
+    debug(str.format('argv={}', str(argv)))
 
     # Parse command line args
     args = parser.parse_args(argv[1:])
@@ -78,9 +199,29 @@ def main(argv):
 
     debug('args=%s' % args)
 
-    rsync('/Users/mcompton/Work/Simpson/src/devcsdata/',
-        ['srcCSDataAccessManager/ProjectCustomFieldSortCriteria.cs'],
-        '/Users/mcompton/Work/Simpson/src/srcBackup/')
+    repo = 'devcsdata/'
+
+    dir_to_backup = path.join('/Users/mcompton/Work/Simpson/src', repo)
+
+    backup_base_dirpath = path.join('/Users/mcompton/Work/Simpson/srcBackup/', repo)
+
+    uncommitted_files_dirpath = path.join(backup_base_dirpath, 'uncommittedFiles')
+    uncommitted_diff_dirpath = path.join(backup_base_dirpath, 'uncommittedDiff')
+    unpushed_commits_dirpath = path.join(backup_base_dirpath, 'unpushedCommits/')
+
+
+    rev_hash, rev_date = hg_newest_unpushed_commit(dir_to_backup)
+
+    if rev_hash != None:
+        bundle_filename = path.join(unpushed_commits_dirpath, make_hg_bundle_filename(rev_hash, rev_date))
+
+        if not path.exists(bundle_filename):
+            hg_bundle_unpushed_commits(dir_to_backup, bundle_filename)
+
+    files_to_backup = hg_list_uncommitted_files(dir_to_backup)
+
+    rsync(dir_to_backup, files_to_backup, uncommitted_files_dirpath)
+
 
 
 if __name__ == "__main__":
