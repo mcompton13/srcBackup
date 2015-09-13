bebfa6fe057ad60a3bbb4f7ad1f5b90b58a32619
Matthew Compton <comptonma@gmail.com>
2015-09-12 23:20:30 -0700

    Added support for saving diffs of uncommited changes.

--- srcBackup.py
+++ srcBackup.py
@@ -13,6 +13,7 @@ INCLUDE_FILE_FORMAT = '+ {}\n'
 INCLUDE_DIR_FORMAT = '+ {}/\n'
 EXCLUDE_ALL = '- *\n'
 
+DIFF_FILENAME_FORMAT = '{}_{}.patch'
 BUNDLE_FILENAME_FORMAT = 'bundle_{}_{}.hg'
 
 HG_OUTGOING_TEMPLATE_FORMAT = '{}{{node|short}}\\n{}{{date|rfc3339date}}'
@@ -83,6 +84,27 @@ def to_utc_datetime(local_datetime):
     return utc_datetime
 
 
+def hg_current_rev(repo_dir_path):
+
+    args = [ "hg",
+        "identify",
+        "--repository",
+        repo_dir_path,
+        "--id",
+    ]
+
+    debug("Calling hg: " + str(args))
+
+    current_rev_hash = None
+
+    try:
+        current_rev_hash = subprocess.check_output(args).strip()
+    except Exception:
+        pass
+
+    return current_rev_hash
+
+
 def hg_newest_unpushed_commit(repo_dir_path):
 
     args = [ "hg",
@@ -102,7 +124,12 @@ def hg_newest_unpushed_commit(repo_dir_path):
 
     debug("Calling hg: " + str(args))
 
-    output_lines = subprocess.check_output(args).strip().split('\n')
+    output_lines = []
+
+    try:
+        output_lines = subprocess.check_output(args).strip().split('\n')
+    except Exception:
+        pass
 
     rev_hash = None
     rev_utc_datetime = None
@@ -124,6 +151,10 @@ def hg_newest_unpushed_commit(repo_dir_path):
 
 def hg_bundle_unpushed_commits(repo_dir_path, destination_bundle_filename):
 
+    # Make sure the bundle hasn't already been backed up
+    if path.exists(destination_dir_path):
+        return
+
     destination_dir_path = path.dirname(destination_bundle_filename)
     if not path.exists(destination_dir_path):
         makedirs(destination_dir_path, 0777)
@@ -132,6 +163,9 @@ def hg_bundle_unpushed_commits(repo_dir_path, destination_bundle_filename):
         "bundle",
         "--repository",
         repo_dir_path,
+        # Don't need compression
+        "--type",
+        "none",
     ]
 
     if is_verbose:
@@ -143,8 +177,31 @@ def hg_bundle_unpushed_commits(repo_dir_path, destination_bundle_filename):
 
     debug("Calling hg: " + str(args))
 
-    result = subprocess.check_output(args).strip().split('\n')
+    try:
+        subprocess.check_output(args)
+    except Exception, ex:
+        error(ex)
 
+
+def hg_diff_uncommitted_files(repo_dir_path, destination_diff_filename):
+    if path.exists(destination_diff_filename):
+        return
+
+    destination_dir_path = path.dirname(destination_diff_filename)
+    if not path.exists(destination_dir_path):
+        makedirs(destination_dir_path, 0777)
+
+    with open(destination_diff_filename, 'w') as f:
+        args = [ "hg",
+            "diff",
+        ]
+
+        debug("Calling hg: " + str(args))
+
+        try:
+            subprocess.call(args, stdout=f)
+        except Exception, ex:
+            error(ex)
 
 
 def make_hg_bundle_filename(commit_rev_hash, commit_datetime_utc):
@@ -155,8 +212,19 @@ def make_hg_bundle_filename(commit_rev_hash, commit_datetime_utc):
     return result
 
 
+def make_hg_diff_filename(commit_rev_hash):
+    commit_rev_hash = commit_rev_hash[0:12]
+    result = str.format(DIFF_FILENAME_FORMAT, datetime.utcnow().isoformat(), commit_rev_hash)
+
+    debug('Bundle filename: ' + result)
+
+    return result
+
 
 def rsync(source_dir_path, source_files, destination_dir_path):
+
+    if not source_files:
+        return
 
     with NamedTemporaryFile() as tmpfile:
 
@@ -185,7 +253,10 @@ def rsync(source_dir_path, source_files, destination_dir_path):
 
         debug("Calling rsync: " + str(args))
 
-        result = subprocess.call(args)
+        try:
+            subprocess.call(args)
+        except Exception, ex:
+            error(ex)
 
 
 def main(argv):
@@ -201,26 +272,33 @@ def main(argv):
 
     repo = 'devcsdata/'
 
-    dir_to_backup = path.join('/Users/mcompton/Work/Simpson/src', repo)
+    repo_to_backup = path.join('/Users/mcompton/Work/Simpson/src/', repo)
 
     backup_base_dirpath = path.join('/Users/mcompton/Work/Simpson/srcBackup/', repo)
 
-    uncommitted_files_dirpath = path.join(backup_base_dirpath, 'uncommittedFiles')
-    uncommitted_diff_dirpath = path.join(backup_base_dirpath, 'uncommittedDiff')
+    uncommitted_files_dirpath = path.join(backup_base_dirpath, 'uncommittedFiles/')
+    uncommitted_diffs_dirpath = path.join(backup_base_dirpath, 'uncommittedDiffs/')
     unpushed_commits_dirpath = path.join(backup_base_dirpath, 'unpushedCommits/')
 
 
-    rev_hash, rev_date = hg_newest_unpushed_commit(dir_to_backup)
+    rev_hash, rev_date = hg_newest_unpushed_commit(repo_to_backup)
 
-    if rev_hash != None:
+    if rev_hash:
         bundle_filename = path.join(unpushed_commits_dirpath, make_hg_bundle_filename(rev_hash, rev_date))
 
         if not path.exists(bundle_filename):
-            hg_bundle_unpushed_commits(dir_to_backup, bundle_filename)
+            hg_bundle_unpushed_commits(repo_to_backup, bundle_filename)
 
-    files_to_backup = hg_list_uncommitted_files(dir_to_backup)
+    current_rev_hash = hg_current_rev(repo_to_backup)
 
-    rsync(dir_to_backup, files_to_backup, uncommitted_files_dirpath)
+    # Hg adds a plus to the end of the revision hash if there are changes
+    if current_rev_hash.endswith('+'):
+        diff_filename = path.join(uncommitted_diffs_dirpath, make_hg_diff_filename(current_rev_hash))
+        hg_diff_uncommitted_files(repo_to_backup, diff_filename)
+
+    files_to_backup = hg_list_uncommitted_files(repo_to_backup)
+
+    rsync(repo_to_backup, files_to_backup, uncommitted_files_dirpath)
 
 
 
